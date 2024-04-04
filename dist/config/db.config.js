15edee1750a0e8f05b2eddb99a65a6157f499436
tirupathirao0201 <124332583+tirupathirao0201@users.noreply.github.com>
2024-04-04 19:20:12 +0530

    db prod

--- dist/config/db.config.js
+++ dist/config/db.config.js
@@ -5,9 +5,10 @@ const typeorm_1 = require("typeorm");
 const entities_1 = require("../entities");
 exports.dataSource = new typeorm_1.DataSource({
     type: 'postgres',
-    password: 'Thiru@2726',
-    host: 'localhost',
-    database: 'expressTS',
+    username: 'tirupathi_rao',
+    password: 'jAvG7w5qXT6WveqsEhmG6fUsLCQiDPAm',
+    host: 'postgres://@dpg-co7ao4md3nmc73e7bo4g-a.oregon-postgres.render.com',
+    database: 'express_ts',
     port: 5432,
     synchronize: true,
     entities: [...entities_1.entities],
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -5,7 +5,7 @@ export const dataSource = new DataSource({
   type: 'postgres',
   username:'tirupathi_rao',
   password: 'jAvG7w5qXT6WveqsEhmG6fUsLCQiDPAm',
-  host: 'localhost',
+  host: 'postgres://@dpg-co7ao4md3nmc73e7bo4g-a.oregon-postgres.render.com',
   database: 'express_ts',
   port: 5432,
   synchronize: true,
