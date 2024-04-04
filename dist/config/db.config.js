40fa68dd7be589afa83a1c83d41a8a28c6c1fd43
tirupathirao0201 <124332583+tirupathirao0201@users.noreply.github.com>
2024-04-04 20:03:29 +0530

    fix: url

--- dist/config/db.config.js
+++ dist/config/db.config.js
@@ -5,10 +5,7 @@ const typeorm_1 = require("typeorm");
 const entities_1 = require("../entities");
 exports.dataSource = new typeorm_1.DataSource({
     type: 'postgres',
-    username: 'tirupathi_rao',
-    password: 'jAvG7w5qXT6WveqsEhmG6fUsLCQiDPAm',
     url: 'postgres://tirupathi_rao:jAvG7w5qXT6WveqsEhmG6fUsLCQiDPAm@dpg-co7ao4md3nmc73e7bo4g-a.oregon-postgres.render.com/express_ts',
-    database: 'express_ts',
     port: 5432,
     synchronize: true,
     entities: [...entities_1.entities],
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,10 +3,7 @@ import {entities} from '../entities'
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  username:'tirupathi_rao',
-  password: 'jAvG7w5qXT6WveqsEhmG6fUsLCQiDPAm',
   url:'postgres://tirupathi_rao:jAvG7w5qXT6WveqsEhmG6fUsLCQiDPAm@dpg-co7ao4md3nmc73e7bo4g-a.oregon-postgres.render.com/express_ts',
-  database: 'express_ts',
   port: 5432,
   synchronize: true,
   entities: [...entities],
